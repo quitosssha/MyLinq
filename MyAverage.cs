331fe388b01b19e3ffc209480f75a7919265d162
Michael <Michael@LENOVO-DESKTOP>
2024-03-09 20:30:02 +0500

    Cleared unnecessary usings

--- MyAverage.cs
+++ MyAverage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyLinq;
+﻿namespace MyLinq;
 
 public static partial class MyLinq
 {
